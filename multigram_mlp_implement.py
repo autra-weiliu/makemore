32ba19c09c526898b9d104a6d1b1c609f4f70910
Wei Liu <wei.liu@autra.tech>
2024-01-06 12:37:51 +0800

    chore: add new todo

---
+++ multigram_mlp_implement.py
@@ -0,0 +1,10 @@
+"""
+Implement multigram mlp next character model
+
+TODO
+1. implement model
+2. implement data & grad distribution & (lr * grad / data) visualization
+3. try weight & activation gain & norm, compared with BN
+4. implement linear & BN & activation
+"""
+import torch
