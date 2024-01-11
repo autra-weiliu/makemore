73f3c32179c01baed499b0b24ab16a19db32fd05
Wei Liu <wei.liu@autra.tech>
2024-01-11 14:30:08 +0800

    feat: implement loss and model

--- multigram_mlp_implement.py
+++ multigram_mlp_implement.py
@@ -8,13 +8,33 @@ from tqdm import tqdm
 SEP = '#'
 
 class MLP(torch.nn.Module):
-    def __init__(self, n_gram: int = 3):
+    def __init__(self, n_gram: int = 3, embed_dim: int = 10, hidden_layer_dim: int = 1000):
         super().__init__()
+        # update model meta
+        self._total_ch = 27
         self._n_gram = n_gram
-
+        self._embed_dim = embed_dim
+        self._hidden_layer_dim = hidden_layer_dim
+        # embedding
+        self._embed = torch.nn.Embedding(num_embeddings=self._total_ch, embedding_dim=embed_dim)
+        # layer1
+        self._linear1 = torch.nn.Linear(in_features=self._n_gram * embed_dim, out_features=hidden_layer_dim)
+        self._bn1 = torch.nn.BatchNorm1d(num_features=hidden_layer_dim)
+        self._act1 = torch.nn.ReLU()
+        # layer2
+        self._linear2 = torch.nn.Linear(in_features=self._hidden_layer_dim, out_features=self._total_ch)
+        
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
-        # TODO(wei.liu) implement model forward with embedding & linear & bn & relu
-        pass
+        # embedding
+        out = self._embed(input_tensor)
+        out = out.reshape((out.shape[0], -1))
+        # layer1
+        out = self._linear1(out)
+        out = self._bn1(out)
+        out = self._act1(out)
+        # layer2
+        out = self._linear2(out)
+        return out
 
 def load_words(filepath: str) -> List[str]:
     with open(filepath, 'r') as f:
@@ -46,13 +66,17 @@ def build_dataset(words: List[str], n_gram: int = 3) -> Tuple[Dict[str, int], Di
 
 # loss
 def loss(model_output: torch.Tensor, gt_output: torch.Tensor) -> torch.Tensor:
-    # TODO implement cross entropy loss
-    return torch.tensor(0)
+    model_output_softmax = F.softmax(model_output, dim=-1)
+    logits = model_output_softmax[torch.arange(gt_output.shape[0]), gt_output]
+    scalar_loss = - logits.log().mean()
+    return scalar_loss
 
 # training config
 n_gram = 3
-lr = 0.01
-log_iter_interval = 10
+lr = 0.001
+lr_decay_rate = 0.99
+lr_decay_iter = 10000
+log_iter_interval = 1000
 dataset_limit = -1
 total_epoch, batch_size = 10, 32
 
@@ -67,6 +91,7 @@ train_label_torch = train_label_torch[: dataset_limit] if dataset_limit > 0 else
 
 # Training loop
 generator = torch.Generator()
+cur_iter = 0
 for epoch in range(1, total_epoch+1):
     # shuffle data indexes
     generator.manual_seed(epoch)
@@ -80,8 +105,11 @@ for epoch in range(1, total_epoch+1):
         loss_value = scalar_loss.item()
         scalar_loss.backward()
         for param in model.parameters():
-            if param.grad and param.requires_grad:
+            if param.grad is not None and param.requires_grad:
                 param.data -= lr * param.grad
         model.zero_grad()
         if iter % log_iter_interval == 0:
-            print(f'epoch: {epoch}, iter: {iter}, loss: {scalar_loss}')
+            print(f'epoch: {epoch}, iter: {iter}, loss: {scalar_loss}, lr: {lr}')
+        cur_iter += 1
+        if cur_iter % lr_decay_iter == 0:
+            lr = lr * lr_decay_rate
