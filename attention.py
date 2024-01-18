acee435bdc122c3adbd01ced278dd5c4a24fb2eb
Wei Liu <wei.liu@autra.tech>
2024-01-18 19:23:19 +0800

    chore: add more todos

---
+++ attention.py
@@ -0,0 +1,7 @@
+# https://zh.d2l.ai/chapter_attention-mechanisms/attention-scoring-functions.html#sec-attention-scoring-functions
+# TODO(wei.liu) implement all the stuffs above
+# 1. masked attention
+# 2. scaled dot product attention
+# 3. additive attention
+
+import torch
--- nw_kernel_regression.py
+++ nw_kernel_regression.py
@@ -1,6 +1,7 @@
 import torch
 
 # TODO implement it
+# https://zh.d2l.ai/chapter_attention-mechanisms/nadaraya-waston.html#id6
 class NWKernelRegression(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
