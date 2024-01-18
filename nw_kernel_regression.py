a475dbdb930a62d1120cd4d72606b08b1ff20a7f
Wei Liu <wei.liu@autra.tech>
2024-01-18 18:13:04 +0800

    chore: add todo for nw kernel regression

---
+++ nw_kernel_regression.py
@@ -0,0 +1,9 @@
+import torch
+
+# TODO implement it
+class NWKernelRegression(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(input_tensor: torch.Tensor) -> torch.Tensor:
+        pass
