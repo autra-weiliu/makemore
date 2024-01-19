e3ae6a1889daa3e6551a2adcda9e41096616700e
Wei Liu <wei.liu@autra.tech>
2024-01-19 20:13:36 +0800

    chore: add more todo

--- attention.py
+++ attention.py
@@ -1,7 +1,14 @@
+# TODO implement all the attentions
+
 # https://zh.d2l.ai/chapter_attention-mechanisms/attention-scoring-functions.html#sec-attention-scoring-functions
-# TODO(wei.liu) implement all the stuffs above
 # 1. masked attention
 # 2. scaled dot product attention
 # 3. additive attention
+
+# https://zh.d2l.ai/chapter_attention-mechanisms/multihead-attention.html
+# 4. multi head scaled dot product attention
+
+# https://zh.d2l.ai/chapter_attention-mechanisms/transformer.html
+# 5. transformer, compared with makemore's implementation
 
 import torch
